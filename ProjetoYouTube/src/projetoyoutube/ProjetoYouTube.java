a3446be5d7e01e26c053119a8304795800f1fb0d
jwictor <39440505+jwictor@users.noreply.github.com>
2018-05-19 15:09:55 -0300

    Add files via upload

---
+++ ProjetoYouTube/src/projetoyoutube/ProjetoYouTube.java
@@ -0,0 +1,18 @@
+
+package projetoyoutube;
+
+
+public class ProjetoYouTube {
+
+        public static void main(String[] args) {
+        // TODO code application logic here
+        Video v[] = new Video[3];
+        v[0] = new Video("Aula 1 de POO");
+        v[1]= new Video("Aula 12 de PHP");
+        v[2]= new Video("Aula 10 de HTML");
+        
+            System.out.println(v[0].toString());
+        
+    }
+    
+}
